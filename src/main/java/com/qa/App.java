945440c030b30bdbdeb26d2984978dde1b03e233
zzziyi <wenziyichen@gmail.com>
2023-10-30 13:48:13 +0000

    Ziyi addition

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Ziyi"));
         System.out.println(sayGoodbye());
     }
 
